c7fd8aa1f5101ca02c0b41901ac3d804e5ac5711
Son Pham-Dinh <sonpd2@viettel.com.vn>
2021-02-18 14:51:35 +0700

    add GA

--- src/App.js
+++ src/App.js
@@ -5,6 +5,13 @@ import LayoutApp from './components/LayoutApp.js';
 import ReactDOM from 'react-dom';
 import ReverseShell from './components/ReverseShell.js';
 import './assets/css/style.css';
+import ReactGA from 'react-ga';
+
+
+function initializeReactGA() {
+    ReactGA.initialize('G-20RZL4GDV4');
+    ReactGA.pageview('/');
+}
 
 const App = () => {
 	return (
